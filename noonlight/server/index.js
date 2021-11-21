d8c40280a82495fee0b5fb23434159924fa78acc
Michael <michael.stoltejr@gmail.com>
2021-11-20 23:52:59 -0700

    initial commit

---
+++ noonlight/server/index.js
@@ -0,0 +1,49 @@
+/*
+The MIT License (MIT)
+ Original Copyright 2018 Phil Nash
+ Modifications and addtions Copyright (c) 2015 Sonos, Inc.
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+ const express = require('express');
+ const axios = require('axios');
+
+ // Load Configuration
+let rawconfig = fs.readFileSync('../../data/options.json');  
+let config = JSON.parse(rawconfig);  
+
+console.log("Starting with configuration:", config)
+
+// HA API http://supervisor/core/api
+// HA WS API http://supervisor/core/websocket
+
+
+
+const app = express();
+app.use(express.json());
+
+app.get('/test', async (req, res) => {
+    axios.post('http://supervisor/core/api/states/input_text.noonlight_alarm_id',{
+        data: {
+            "state": `Random Number: ${Math.floor(Math.random()*100)}`
+        }
+    })
+});
+
+app.listen(5950, () =>
+  console.log('Express server is running on localhost:5950')
+);
