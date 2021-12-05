fcfc068befae2ad744d1f4fc126e50d891a51802
Michael <michael.stoltejr@gmail.com>
2021-12-04 19:49:32 -0700

    updated log2

--- noonlight/server/index.js
+++ noonlight/server/index.js
@@ -126,7 +126,7 @@ app.use(express.json());
 //   pin: '9658'
 
 app.post('/createAlarm', async (req, res) => {
-    console.log(req.body);
+    console.log(JSON.stringify(req));
     // axios.post('https://api-sandbox.noonlight.com/dispatch/v1/alarms',req.data).then(resp => {
     //     const {id, status, created_at, owner_id} = resp.data;
 
